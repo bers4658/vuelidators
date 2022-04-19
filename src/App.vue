2eddd754c475b22065c86c52c27773e46a83b848
Bers46582 <gorbunov4658@yandex.ru>
2022-04-19 13:05:48 +0300

    Валидация

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,112 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <form class="pt-3" @submit.prevent="onSubmit">
+      <div class="form-group pt-3">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email" 
+          class="form-control"
+          :class="{'is-invalid': $v.email.$error}"
+          @blur="$v.email.$touch()"
+          v-model="email"
+          > 
+          <div  class="invalid-feedback" v-if="!$v.email.required">Email field is required</div>
+          <div  class="invalid-feedback" v-if="!$v.email.email">This field should be an email</div>
+          <div  class="invalid-feedback" v-if="!$v.email.uniqEmail">This emai is already exists</div>
+      </div>
+
+      <div class="form-group pt-3">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password" 
+          class="form-control"
+          :class="{'is-invalid': $v.password.$error}"
+          @blur="$v.password.$touch()"
+          v-model="password"
+          > 
+          <div  class="invalid-feedback" v-if="!$v.email.minLength">
+            Min length of password is {{ $v.password.$params.minLength.min }}. Now it is {{ password.length }}
+          </div>
+      </div>
+
+      <div class="form-group pt-3">
+        <label for="confirm">Confirm pasword</label>
+        <input
+          type="password"
+          id="confirm" 
+          class="form-control"
+          :class="{'is-invalid': $v.confirmPassword.$error}"
+          @blur="$v.confirmPassword.$touch()"
+          v-model="confirmPassword"
+          > 
+          <div  class="invalid-feedback" v-if="!$v.confirmPassword.sameAs">
+            Passwords should match
+          </div>
+      </div>
+       <!-- {{ $v }} -->
+
+       <button
+        class="btn btn-success mt-3"
+        :disabled="$v.$invalid"
+        >Submit</button>
+    </form>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
 
 export default {
-  name: 'App',
+  data () {
+    return {
+      form: {
+        password: '',
+      },
+        email: '',
+        password: '',
+        confirmPassword: '',
+        
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('Email', this.email);
+      console.log('Password', this.password);
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email,
+      uniqEmail: function(newEmail) {
+        if (newEmail === '') return true
+
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            const value = newEmail !== 'test@mail.ru'
+            resolve(value)
+          }, 1000)
+        } )
+      }
+    },
+    password: {
+      minLength: minLength(6)
+    },
+    confirmPassword: {
+      sameAs: sameAs('password')
+    }
+  },
+
   components: {
-    HelloWorld
+
   }
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
